e40560db32dcf20716d4b0efb86302fdc3097865
Thomaswalsh1 <130932404+thomaswalsh1@users.noreply.github.com.>
2026-06-02 10:37:00 -0400

    Modifying interested section

--- src/sections/Interested.tsx
+++ src/sections/Interested.tsx
@@ -9,7 +9,7 @@ export default function Interested() {
                     < RiUser3Line className="absolute right-4 w-32 h-32 lg:w-82 lg:h-82 lg:right-[10%] opacity-20 z-5" />
                 </div>
                 <span className="text-xl md:text-2xl md:text-left max-w-[700px]">
-                    Thomas is currently interested in a <b>wide variety of roles.</b> Whether it be Software Engineering, Development, Front end, Back end, or Database Engineering, <b>Thomas is ready to apply his skills to meet your goals.</b>
+                    Thomas is currently interested in a <b>wide variety of roles.</b> Feel free to reach out via email.
                 </span>
             </div>
         </div>
